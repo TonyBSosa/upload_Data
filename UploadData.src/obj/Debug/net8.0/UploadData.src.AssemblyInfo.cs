32f609eac8fcb655076b9b4421228e91f8960037
Christell Gamez Garcia <156734210+Chrissgamez@users.noreply.github.com>
2025-09-11 22:16:42 -0600

    Subiendo mi proyecto

--- UploadData.src/obj/Debug/net8.0/UploadData.src.AssemblyInfo.cs
+++ UploadData.src/obj/Debug/net8.0/UploadData.src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UploadData.src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d8dab4678744404ac1378e8630fa60cc34964b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e6fbfb167613d2fcd63e233664287ff231b7fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("UploadData.src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UploadData.src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
